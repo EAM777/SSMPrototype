c4f562122d225f81bee5e4b89e78d37233d66101
Elliot <elliot@MacBook-Pro.local>
2017-09-25 18:25:39 -0700

    set up face Email Authentication

--- SSMPrototype/AppDelegate.swift
+++ SSMPrototype/AppDelegate.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import Firebase
+import FBSDKLoginKit
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,9 +20,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                      didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?)
         -> Bool {
             FirebaseApp.configure()
+            
+            FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
+
             return true
     }
-}
+
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
@@ -44,6 +49,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        return FBSDKApplicationDelegate.sharedInstance().application(application, open: url, sourceApplication: sourceApplication, annotation: annotation)
+
+    }
+}
 
 
 
--- SSMPrototype/Controller/SignInVC.swift
+++ SSMPrototype/Controller/SignInVC.swift
@@ -7,9 +7,14 @@
 //
 
 import UIKit
+import Firebase
+import FBSDKCoreKit
+import FBSDKLoginKit
 
 class SignInVC: UIViewController {
-
+    @IBOutlet weak var emailField: TxtField!
+    @IBOutlet weak var pwdField: TxtField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,60 @@ class SignInVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func FacebookBtnTap(_ sender: AnyObject) {
+        
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if  error != nil {
+                print("SSMPT: Unable to authenticate with Facebook - \(String(describing: error))")
+            } else if result?.isCancelled == true {
+                print("SSMPT: User cancelled Facebook Authentication")
+                
+            } else {
+                print("SSMPT: Successfully authenticated with Facebook")
+                let credential = FacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+        }
+        
+    }
+    
+    func firebaseAuth(_ credential: AuthCredential) {
+        Auth.auth().signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("SSMPT: Unable to authenticate with Firebase - \(String(describing: error))")
+            } else {
+                print("SSMPT: Successfully authenticated with Firebase")
+            
+            }
+            
+            
+            } )
+        
+        }
+    @IBAction func signInTap(_ sender: AnyObject) {
+        if let email = emailField.text, let pwd = pwdField.text {
+            Auth.auth().signIn(withEmail: email, password: pwd, completion: { (user, error) in
+                if error == nil {
+                    print("SSMPT: Email user authenticated with Firebase")
+                } else {
+                    Auth.auth().createUser(withEmail: email, password: pwd, completion: { (user, error) in
+                        if error != nil {
+                            print("SSMTP: Unable to Authorize with Firebase using email")
+                        } else {
+                            print("SSMPT: successfully authenticated with Firebase")
+                        }
+                        
+                    })
+                }
+                
+                
+            })
+        }
+    }
+    
 }
+
+
 
