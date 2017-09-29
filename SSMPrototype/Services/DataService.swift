a96f1abd8e271def9ef0ba8b914654e2eb89ca29
Elliot <elliot@MacBook-Pro.local>
2017-09-28 17:22:12 -0700

    completed getting firebase data

--- SSMPrototype/Services/DataService.swift
+++ SSMPrototype/Services/DataService.swift
@@ -17,14 +17,14 @@ class DataService {
     static let ds = DataService()
     
     private var _REF_BASE = DB_BASE
-    private var _REF_POST = DB_BASE.child("post")
+    private var _REF_POSTS = DB_BASE.child("post")
     private var _REF_USERS = DB_BASE.child("users")
     
     var REF_BASE: DatabaseReference {
         return _REF_BASE
     }
     
-    var REF_POST: DatabaseReference {
+    var REF_POSTS: DatabaseReference {
         return _REF_BASE
     }
     
