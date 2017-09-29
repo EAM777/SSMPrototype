cf37ab82a4f724d0ce6ade23d31dc81f4fac1f0e
Elliot <elliot@MacBook-Pro.local>
2017-09-28 18:01:15 -0700

    Firebase Get Complete

--- SSMPrototype/Controller/FeedVC.swift
+++ SSMPrototype/Controller/FeedVC.swift
@@ -14,6 +14,7 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
     
+    var post = [Post]()
 
 
     override func viewDidLoad() {
@@ -23,7 +24,20 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         tableView.dataSource = self
         
         DataService.ds.REF_POSTS.observe(.value, with: { (snapshot) in
-            print(snapshot.value)
+            if let snapshot = snapshot.children.allObjects as? [DataSnapshot] {
+                
+                for snap in snapshot {
+                    print("SNAP: \(snap)")
+                    if let postDict = snap.value as? Dictionary<String, AnyObject> {
+                        let key = snap.key
+                        let post = Post(postKey: key, postData: postDict)
+                        self.post.append(post)
+                    }
+                }
+                
+            }
+            
+            self.tableView.reloadData()
         })
     
         
@@ -34,9 +48,12 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return post.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let post = Post[indexPath.row]
+        print("SSMPT: \(post.caption)")
         return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
     }
     
